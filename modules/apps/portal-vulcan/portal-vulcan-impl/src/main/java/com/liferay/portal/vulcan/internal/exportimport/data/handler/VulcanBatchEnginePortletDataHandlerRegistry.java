dbdd0d4646d551bdb2663f38407476290e5106d2
ZakiMiklos <miklos.zakanyi@liferay.com>
2025-01-21 18:28:29 +0100

    Revert "LPD-45048 Hiding behind system dev feature flag LPD-35914"
    This reverts commit 10fbef160638cae9d9493bb979e3e7634ecd7257.

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/exportimport/data/handler/VulcanBatchEnginePortletDataHandlerRegistry.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/exportimport/data/handler/VulcanBatchEnginePortletDataHandlerRegistry.java
@@ -11,11 +11,9 @@ import com.liferay.batch.engine.service.BatchEngineExportTaskService;
 import com.liferay.batch.engine.service.BatchEngineImportTaskService;
 import com.liferay.exportimport.kernel.lar.PortletDataHandler;
 import com.liferay.osgi.util.ServiceTrackerFactory;
-import com.liferay.portal.kernel.feature.flag.FeatureFlagListener;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
-import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.vulcan.batch.engine.VulcanBatchEngineTaskItemDelegate;
 
@@ -40,27 +38,14 @@ public class VulcanBatchEnginePortletDataHandlerRegistry {
 
 	@Activate
 	protected void activate(BundleContext bundleContext) {
-		_serviceTracker = ServiceTrackerFactory.create(
+		_serviceTracker = ServiceTrackerFactory.open(
 			bundleContext, "(batch.engine.task.item.delegate=true)",
 			new VulcanBatchEngineTaskItemDelegateServiceTrackerCustomizer(
 				bundleContext));
-
-		_serviceRegistration = bundleContext.registerService(
-			FeatureFlagListener.class,
-			(companyId, featureFlagKey, enabled) -> {
-				if (enabled) {
-					_serviceTracker.open();
-				}
-				else {
-					_serviceTracker.close();
-				}
-			},
-			MapUtil.singletonDictionary("featureFlagKey", "LPD-35914"));
 	}
 
 	@Deactivate
 	protected void deactivate() {
-		_serviceRegistration.unregister();
 		_serviceTracker.close();
 
 		for (ServiceRegistration<PortletDataHandler> serviceRegistration :
@@ -92,7 +77,6 @@ public class VulcanBatchEnginePortletDataHandlerRegistry {
 	@Reference
 	private BatchEngineImportTaskService _batchEngineImportTaskService;
 
-	private ServiceRegistration<FeatureFlagListener> _serviceRegistration;
 	private final Map
 		<ServiceReference<VulcanBatchEngineTaskItemDelegate>,
 		 ServiceRegistration<PortletDataHandler>> _serviceRegistrations =
