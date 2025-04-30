85dfc9ce19921e9fdd1b739d5e7b3fa81a6619dd
ZakiMiklos <miklos.zakanyi@liferay.com>
2025-04-30 09:22:59 +0200

    adfsds

--- modules/test/playwright/tests/export-import-web/env/set_up.sh
+++ modules/test/playwright/tests/export-import-web/env/set_up.sh
@@ -7,9 +7,8 @@ echo CURRENT_DIR_NAME=${CURRENT_DIR_NAME}
 source ${CURRENT_DIR_NAME}/../../../env/common.sh
 
 function main {
-	local liferay_home=${1}
 
-	export TOMCAT_DIR=$(get_tomcat_dir ${liferay_home})
+	export LIFERAY_HOME=${LIFERAY_HOME}
 
 	default_set_up
 
